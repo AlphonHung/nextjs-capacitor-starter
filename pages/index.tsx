6a1831fdecb0855b3f6cfe47ea035eb3e4297b6f
DesmondHung <alphon.hung@gmail.com>
2021-12-01 00:11:42 +0800

    [Modify] animate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import Layout from '~/components/layout/Layout';
-import { Animated } from "react-animated-css";
+// import { Animated } from "react-animated-css";
 
 /** Home頁會取得getStaticProps的返回內容(allPostsData) */
 export default function Home({ allPostsData }) {
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
                 <div className={`w-full h-1/5 flex justify-center items-center bg-gray-400 text-xl`}>誠心祈求後點我求籤</div>
                 <div className={`w-full h-1/5 flex justify-center items-center bg-gray-200 text-xl`} onClick={() => { setShowIntro(!showIntro) }}>張王爺求籤簡介</div>
                 <div className={`bg-temple bg-cover flex justify-center items-center flex-1`}>
-                    <Animated animationIn="fadeInDown" animationOut="fadeOutUp" animationInDuration={500} animationOutDuration={500} isVisible={showIntro}>
+                    <div className={`animated duration-500 ${showIntro ? 'fadeInDown' : 'fadeOutUp'}`}>
                         {init && <div className={`flex flex-col justify-center items-center text-white text-lg`}>
                             <p>王爺本宮原位於恆春古城</p>
                             <p>93年為服務大眾遷移高雄</p>
@@ -28,7 +28,7 @@ export default function Home({ allPostsData }) {
                             <p>製造手機求籤方便大眾</p>
                             <p>勸善懲惡</p>
                         </div>}
-                    </Animated>
+                    </div>
                 </div>
             </div>
         </Layout>
